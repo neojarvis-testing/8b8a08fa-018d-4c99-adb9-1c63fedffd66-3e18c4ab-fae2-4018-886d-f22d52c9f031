a03b62fe80f107a5bf4273b098bc515da215bc93
pallenithin <nithinpalle163@gmail.com>
2025-04-28 06:05:20 +0000

    adminclasscomponent

--- angularapp/src/app/components/adminaddclass/adminaddclass.component.ts
+++ angularapp/src/app/components/adminaddclass/adminaddclass.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-adminaddclass',
   templateUrl: './adminaddclass.component.html',
   styleUrls: ['./adminaddclass.component.css']
 })
-export class AdminaddclassComponent implements OnInit {
 
-  constructor() { }
+export class AdminaddclassComponent implements OnInit {
+  existingClasses: string[] = ['Italian Basics', 'French Cuisine']; 
+
+  constructor() {}
 
   ngOnInit(): void {
+
   }
 
+  onSubmit(form: NgForm): void {
+    if (form.invalid) {
+      alert('All fields are required'); 
+      return;
+    }
+
+    const className = form.value.className;
+
+    if (this.existingClasses.includes(className)) {
+      alert('Cooking class with the same name already exists'); 
+    } else {
+      this.existingClasses.push(className); 
+      alert('Successfully Added!'); 
+      form.reset(); 
+    }
+  }
 }
