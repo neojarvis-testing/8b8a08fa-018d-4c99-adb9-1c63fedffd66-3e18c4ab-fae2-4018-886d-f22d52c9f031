f43edbb4b1da70b26ddfadc5a9effdc55d12c5ff
Satyam-k-jha <skumar65321@gmail.com>
2025-05-01 08:19:55 +0000

    changed routing

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfe
 import { UserviewappliedrequestComponent } from './components/userviewappliedrequest/userviewappliedrequest.component';
 import { UserviewclassComponent } from './components/userviewclass/userviewclass.component';
 import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
-import { AuthguardComponent } from './components/authguard/authguard.component';
+import { AuthguardComponent } from './authguard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
